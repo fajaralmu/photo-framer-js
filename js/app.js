5e5f8a4d3e398f1be1902c8db07d3da02645792d
Fajar AM <somabangsa@gmail.com>
2020-09-07 09:57:19 +0700

    init multi image watermarker

--- js/app.js
+++ js/app.js
@@ -2,9 +2,28 @@ function _id(id){
     return document.getElementById(id);
 }
 
+/**
+ * 
+ * @param {*} fileInput input type file
+ * @param {*} callback 
+ */
 function toBase64(fileInput, callback){
 	const reader = new FileReader();
     reader.readAsDataURL(fileInput.files[0]);
+    reader.onload = () => callback(reader.result);
+    reader.onerror = error => {
+    	alert("Error Loading File");
+    }
+}
+
+/**
+ * 
+ * @param {*} file file from iput file
+ * @param {*} callback 
+ */
+function toBase64Multiple(file, callback){
+	const reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onload = () => callback(reader.result);
     reader.onerror = error => {
     	alert("Error Loading File");
