22e036de140904cb93e3e67274786937e4d4ec46
Fajar AM <somabangsa@gmail.com>
2020-10-12 15:59:40 +0700

    image cropping instead of fitting

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function byId(id){
+function byId(id) {
     return document.getElementById(id);
 }
 
@@ -7,12 +7,12 @@ function byId(id){
  * @param {*} fileInput input type file
  * @param {*} callback 
  */
-function toBase64(fileInput, callback){
-	const reader = new FileReader();
+function toBase64(fileInput, callback) {
+    const reader = new FileReader();
     reader.readAsDataURL(fileInput.files[0]);
     reader.onload = () => callback(reader.result);
     reader.onerror = error => {
-    	alert("Error Loading File");
+        alert("Error Loading File");
     }
 }
 
@@ -21,11 +21,61 @@ function toBase64(fileInput, callback){
  * @param {*} file file from iput file
  * @param {*} callback 
  */
-function toBase64Multiple(file, callback){
-	const reader = new FileReader();
+function toBase64Multiple(file, callback) {
+    const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => callback(reader.result);
     reader.onerror = error => {
-    	alert("Error Loading File");
+        alert("Error Loading File");
     }
+}
+
+/**
+ * 
+ * @param {Image} img 
+ */
+function caliberateImageSize(img) {
+    const dimension = {
+        width: WIDTH,
+        height: HEIGHT,
+        adjustedSide: null
+    };
+    const adjust = img.width != img.height;
+
+    if (!adjust) {
+        return dimension;
+    }
+    const w = img.width;
+    const h = img.height;
+
+    var adjustedSide = null;
+    var finalWidth = WIDTH;
+    var finalHeight = HEIGHT;
+    if (w > h) {
+        var multiplier = 1;
+        if (h <= HEIGHT) {
+            multiplier = HEIGHT / h;
+        } else if (h > HEIGHT) {
+            multiplier = HEIGHT / h;
+        }
+        finalWidth = w * multiplier;
+        finalHeight = h * multiplier;
+
+        adjustedSide = "w";
+    } else {
+        var multiplier = 1;
+        if (w <= WIDTH) {
+            multiplier = WIDTH / w;
+        } else if (w > WIDTH) {
+            multiplier = WIDTH / w;
+        }
+        finalWidth = w * multiplier;
+        finalHeight = h * multiplier;
+
+        adjustedSide = "h";
+    }
+    dimension.height = Math.round(finalHeight);
+    dimension.width = Math.round(finalWidth);
+    dimension.adjustedSide = adjustedSide;
+    return dimension;
 }
