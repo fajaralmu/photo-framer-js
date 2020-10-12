df7230e68a067394ec08e21181d9d0dee8483432
Fajar AM <somabangsa@gmail.com>
2020-10-12 16:09:41 +0700

    add custom dialog

---
+++ js/dialogs.js
@@ -0,0 +1,246 @@
+function confirmDialog(msg, prop) {
+	const property = prop?prop: {
+		yesIcon: null,
+		noIcon: null,
+		yesText: 'Yes',
+		noText: 'No',
+		dialogIcon: null
+	};
+	
+	if(property.yesIcon && property.yesText){
+		property.yesHtml = "<i class=\""+property.yesIcon+"\"></i>&nbsp;"+ property.yesText;
+	}else{
+		property.yesHtml = "<span>Yes</span>";
+	}
+	
+	if(property.noIcon && property.noText){
+		property.noHtml = "<i class=\""+property.noIcon+"\"></i>&nbsp;"+ property.noText;
+	}else{
+		property.noHtml = "<span>No</span>";
+	}
+	
+	return new Promise(function(resolve, reject) {
+		const dialog = createHtmlTag({
+			tagName : 'div',
+			style:{ 'z-index': 6 },
+			ch0 : modalBackdropJson(),
+			ch1 : {
+				tagName : 'div',
+				className : 'modal fade show',
+				role : 'dialog',
+				style : { display : 'block', 'text-align' : 'center', 'margin-top' : '30vh', },
+				ch0 : {
+					tagName : 'div',
+					className : 'modal-dialog',
+					role : "document",
+					ch1 : {
+						tagName : 'div',
+						className : 'modal-content',
+						ch1 : modalHeader("Confirmation"),
+						ch2 : modalBodyGrid('<h3><i class="'+ (property.dialogIcon ? property.dialogIcon: 'fas fa-exclamation-triangle') +'"></i></h3><p>' + msg + '</p>', '20% 70%'),
+						ch3 : {
+							tagName : 'div',
+							className : 'modal-footer',
+							ch1 : {
+								style : { margin : 'auto', style:{width:'max-content'} },
+								ch1:{
+									tagName : 'button',
+									innerHTML : property.yesHtml,
+									className : 'btn btn-primary',
+									style: {margin: '3px'},
+									onclick : function(e) {
+										resolve(true);
+										dialog.parentNode.removeChild(dialog);
+									}
+								},
+								ch2:{
+									tagName : 'button',
+									innerHTML : property.noHtml,
+									style: {margin: '3px'},
+									className : 'btn btn-danger',
+									onclick : function(e) {
+										resolve(false);
+										dialog.parentNode.removeChild(dialog);
+									}
+								},
+							}
+						}
+					}
+				},
+			}
+		})
+
+		document.body.prepend(dialog);
+	});
+}
+
+function promptDialog(msg, inputType) {
+	return new Promise(function(resolve, reject) {
+		const dialog = createHtmlTag({
+			tagName : 'div',
+			style:{ 'z-index': 6 },
+			ch0 : modalBackdropJson(),
+			ch1 : {
+				tagName : 'div',
+				className : 'modal fade show',
+				role : 'dialog',
+				style : { display : 'block', 'text-align' : 'center', 'margin-top' : '30vh', },
+				ch0 : {
+					tagName : 'div',
+					className : 'modal-dialog',
+					role : "document",
+					ch1 : {
+						tagName : 'div',
+						className : 'modal-content',
+						ch1 : modalHeader("Prompt"),
+						ch2 :  modalBody({
+							tagName:'div',
+							ch0:{
+								tagName:'p',
+								innerHTML: msg
+							},
+							ch1: {
+								tagName: 'input',
+								type:inputType?inputType:'text',
+								id: 'prompt-input-val',
+								className: 'form-control'
+							}
+						}),
+						ch3 : {
+							tagName : 'div',
+							className : 'modal-footer',
+							ch1 : {
+								style : { margin : 'auto', style:{width:'max-content'} },
+								ch1:{
+									tagName : 'button',
+									innerHTML : 'Yes',
+									className : 'btn btn-primary',
+									style: {margin: '3px'},
+									onclick : function(e) {
+										let val = byId('prompt-input-val').value;
+										if(!val){
+											alert("Invalid input!");
+											return;
+										}
+										console.log("Prompt Val: ", val);
+										resolve({ok:true, value:val} );
+										dialog.parentNode.removeChild(dialog);
+									}
+								},
+								ch2:{
+									tagName : 'button',
+									innerHTML : 'No',
+									style: {margin: '3px'},
+									className : 'btn btn-secondary',
+									onclick : function(e) {
+										resolve({ok:false, value: null});
+										dialog.parentNode.removeChild(dialog);
+									}
+								},
+							}
+						}
+					}
+				},
+
+			}
+		})
+
+		document.body.prepend(dialog);
+	});
+}
+
+
+function infoDialog(msg) {
+	return new Promise(function(resolve, reject) {
+		const dialog = createHtmlTag({
+			tagName : 'div',
+			style:{ 'z-index': 6 },
+			ch0 : modalBackdropJson(),
+			ch1 : {
+				tagName : 'div',
+				className : 'modal fade show',
+				role : 'dialog',
+				style : { display : 'block', 'text-align' : 'center', 'margin-top' : '30vh', },
+				ch0 : {
+					tagName : 'div',
+					className : 'modal-dialog',
+					role : "document",
+					ch1 : {
+						tagName : 'div',
+						className : 'modal-content',
+						ch1 : modalHeader("Info"),
+						ch2 : modalBody('<h2><i class="fa fa-info-circle"></i></h2>'+ msg),
+						ch3 : {
+							tagName : 'div',
+							className : 'modal-footer',
+							ch1 : {
+								style : { margin : 'auto' },
+								tagName : 'button',
+								innerHTML : 'Ok',
+								className : 'btn btn-primary',
+								onclick : function(e) {
+									resolve(true);
+									dialog.parentNode.removeChild(dialog);
+								}
+							}
+						}
+					}
+				},
+
+			}
+		})
+
+		document.body.prepend(dialog);
+	});
+}
+
+
+function modalHeader(text){
+	return {
+		tagName : 'div', className : 'modal-header',
+		ch1 : {
+			tagName : 'h5',
+			className : 'modal-title',
+			innerHTML : text,
+			style:{margin:'auto'}
+		},
+	};
+}
+
+function modalBackdropJson(){
+	return {
+		tagName : 'div',
+		className : 'modal-backdrop', 
+		style:{ 'background-color': 'rgba(150,150,150,0.5)' }
+	}
+}
+
+function modalBodyGrid(htmlString, gridTemplateColumns){
+	const obj = {
+		tagName : 'div',
+		className : 'modal-body',
+	}
+	 
+	obj.innerHTML = htmlString;
+	obj.style = {
+		'display': 'grid',
+		'gird-template-columns': gridTemplateColumns?gridTemplateColumns:'auto'
+	}
+	  
+	return obj;
+}
+
+function modalBody(html){
+	const obj = {
+		tagName : 'div',
+		className : 'modal-body',
+	}
+	
+	if(typeof(html) == "string"){
+		obj.innerHTML = html; 
+	}else{
+		obj.ch0 = html;
+	}
+	
+	return obj;
+}
---
+++ js/html.js
@@ -0,0 +1,78 @@
+ 
+/**
+ * 
+ * @param styleObject
+ * @returns string of ';' joined style items
+ */
+function stringifyStyleObject(styleObject){
+	const keyValueArrays = new Array();
+	
+	for(key in styleObject){
+		const keyValue  = key+":"+styleObject[key];
+		keyValueArrays.push(keyValue);
+	}
+	return keyValueArrays.join(';');
+}
+
+/**
+ * 
+ * @param object
+ * @returns <DOM>
+ */
+function createHtmlTag(object){
+	if(null == object){
+		object = { tagName: 'span', innerHTML: 'invalid DOM info', style:{color: 'red'}};
+	} 
+	const tag = document.createElement(object.tagName);
+	tag.innerHTML = object["innerHTML"] ? object["innerHTML"] : "";
+	
+	for(let key in object){
+		if(key == 'innerHTML' || key == 'tagName'){
+			continue;
+		}  
+		const value = object[key];
+		const isNotNull = object[key] != null;
+		const isStyle = key == "style";
+		const isObject = isNotNull && typeof(value) ==  "object";
+		const isHtmlElement = isNotNull && value instanceof HTMLElement;
+		const isFunction = isNotNull && typeof(value) ==  "function";
+		const isArray = isNotNull && Array.isArray(value);
+		
+		if(isHtmlElement){
+			tag.appendChild(value);
+//		}else if(isArray){
+//			for (var i = 0; i < value.length; i++) {
+//				const htmlTag = createHtmlTag(value[i]);
+//				tag.appendChild(htmlTag);
+//			}
+		}else if(isObject && !isFunction){
+			if(isStyle){
+				tag.setAttribute(key, stringifyStyleObject(value));
+			}else{ // Html DOM
+			//	console.debug("will create HTML DOM of :", key);
+				const htmlObject = value;
+				const htmlTag = createHtmlTag(htmlObject);
+				tag.appendChild(htmlTag);
+			}
+		}else if(isFunction){
+			 
+			tag[key] = function(e){ value(e) };
+			 
+		}else{
+			if(key == "className"){
+				key = "class";
+			}
+			tag.setAttribute(key, value);
+		}
+	}
+	tag.setAttribute("dynamictag", object.tagName);
+	return tag;
+}
+
+
+function domToString(dom){
+	tempComponent.innerHTML = "";
+	tempComponent.appendChild(dom);
+	
+	return tempComponent.innerHTML;
+}
